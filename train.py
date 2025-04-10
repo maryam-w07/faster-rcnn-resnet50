ccead381a5bb31be28c518a9973dffa9f4cffc74
Maryam <124738989+maryam-w07@users.noreply.github.com>
2025-04-10 09:49:42 +0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,14 @@
+import torch
+
+def train(model, data_loader, optimizer, device):
+    model.train()
+    for images, targets in data_loader:
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
