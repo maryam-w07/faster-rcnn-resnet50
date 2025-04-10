09ce539fbe52609df7744f1d4c20946d25938178
Maryam <124738989+maryam-w07@users.noreply.github.com>
2025-04-10 09:51:20 +0500

    Update model.py

--- model.py
+++ model.py
@@ -7,3 +7,13 @@ def get_model(num_classes):
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = detection.FastRCNNPredictor(in_features, num_classes)
     return model
+# Initialize the model
+num_classes = 2 #
+model = get_model(num_classes)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+
+# Define optimizer and learning rate scheduler
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
